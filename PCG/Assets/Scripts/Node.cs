ed602521503927f81d9f6c792149d4dfd2e2dd00
teresa1 <a10674@alunos.ipca.pt>
2016-12-02 12:48:11 +0000

    funciona ta pronto yay

--- PCG/Assets/Scripts/Node.cs
+++ PCG/Assets/Scripts/Node.cs
@@ -1,128 +1,157 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class Node 
+[Serializable]
+public class Node
 {
+    #region Static Fields and Constants
+
     // the position and size of this Leaf
-    private const int MIN_LEAF_SIZE = 6;
-    public int posX { get; private set; }
-    public int posZ { get; private set; }
-    public int width { get; private set; }
-    public int height { get; private set; }
+    private const int MinLeafSize = 25;
 
-    bool splitH;
-    private int max;
-    public Node leftChild; // the Leaf's left child Leaf
-    public Node rightChild; // the Leaf's right child Leaf
-    public GameObject room; // the room that is inside this Leaf
+    #endregion
 
-    private Node helperNode;
-    private Node root;
+    #region Proprieties
+
+    public int PosX { get { return posX; } }
+    public int PosZ { get { return posZ; } }
+    public int Width { get { return width; } }
+    public int Height { get { return height; } }
+
+    /// <summary>
+    ///     the Leaf's left child Leaf
+    /// </summary>
+    public Node LeftChild { get { return leftChild; } }
+
+    /// <summary>
+    ///     the Leaf's right child Leaf
+    /// </summary>
+    public Node RightChild { get { return rightChild; } }
+
+    /// <summary>
+    ///     the room that is inside this Leaf
+    /// </summary>
+    public GameObject Room { get { return room; } }
+
+    #endregion
+
+    #region Fields
+
+    #region Private Fields
+
+    [SerializeField] private Node leftChild;
+    [SerializeField] private Node rightChild;
+    [SerializeField] private GameObject room;
+
+    [SerializeField] private int posX;
+    [SerializeField] private int posZ;
+    [SerializeField] private int width;
+    [SerializeField] private int height;
+
+    #endregion
+
+    #endregion
+
+    #region Constructors
+
     // public var halls:Vector.; // hallways to connect this Leaf to other Leafs
 
-        
     public Node(int PosX, int PosZ, int Width, int Height)
     {
-
         posX = PosX;
         posZ = PosZ;
         height = Height;
         width = Width;
-
     }
 
-    public bool split(List<Node> nodes)
+    #endregion
+
+    #region Methods
+
+    #region Public Methods
+
+    // begin splitting the leaf into two children
+    public bool Split()
     {
-        // begin splitting the leaf into two children
-        if (leftChild != null || rightChild != null)
-            return false; // we're already split! Abort!
+        // we're already split! Abort!
+        if (LeftChild != null || RightChild != null)
+            return false;
 
         // determine direction of split
         // if the width is >25% larger than height, we split vertically
         // if the height is >25% larger than the width, we split horizontally
         // otherwise we split randomly
 
+        bool splitH = Random.Range(0.0F, 1.0F) > 0.5F;
 
-        if (Random.Range(0, 2) > 0.5) {
-            splitH = true;
-        }
-
-        if (width > height && width/height >= 1.25)
+        if (Width > Height && Width/Height >= 1.25F)
             splitH = false;
-        else if (height > width && height/width >= 1.25)
+        else if (Height > Width && Height/Width >= 1.25F)
             splitH = true;
 
+        int max;
 
-        if (splitH) {
-            max = height - MIN_LEAF_SIZE;
-        }
-        else {
-            max = width - MIN_LEAF_SIZE;
-        }
-        if (max <= MIN_LEAF_SIZE)
-            return false; // the area is too small to split any more...
+        if (splitH) max = Height - MinLeafSize;
+        else max = Width - MinLeafSize;
 
-        int split = Random.Range(MIN_LEAF_SIZE, max); // determine where we're going to split
+        // the area is too small to split any more...
+        if (max <= MinLeafSize)
+            return false;
+
+        // determine where we're going to split
+        int split = Random.Range(MinLeafSize, max);
 
         // create our left and right children based on the direction of the split
         if (splitH) {
-            //leftChild = new Node(posX, posZ, width, split);
-            //rightChild = new Node(posX, posZ + split, width, height - split);
-            leftChild = new Node((int)room.transform.localScale.x,(int) room.transform.localScale.y,(int) room.transform.localScale.x, split);
-            rightChild = new Node(
-                (int)room.transform.position.x,
-                (int)room.transform.position.z - (int)((split - room.transform.localScale.z) / 2), (int)room.transform.localScale.x, split );
+            leftChild = new Node(PosX, PosZ, Width, split);
+            rightChild = new Node(PosX, PosZ + split, Width, Height - split);
         }
         else {
-            //leftChild = new Node(posX, posZ, split, height);
-            //rightChild = new Node(posX + split, posZ, width - split, height);
-            leftChild = new Node((int)room.transform.position.x - (int)((split - room.transform.localScale.x) / 2),
-                                (int)room.transform.position.z, (int)room.transform.localScale.x, (int)room.transform.localScale.z);
+            leftChild = new Node(PosX, PosZ, split, Height);
+            rightChild = new Node(PosX + split, PosZ, Width - split, Height);
+        }
+        // split successful!
 
-            rightChild = new Node((int)(split - (room.transform.localScale.x) / 2),
-               (int) room.transform.position.z, (int)room.transform.localScale.x, (int)room.transform.localScale.z);
-        }
-        Debug.Log(nodes.Count);
-        for (int i = 0; i < nodes.Count; i++) {
-            createRooms();
-        }
-        return true; // split successful!   
+        return true;
     }
 
-
-   
-
-
-    public void createRooms()
+    public void SetRoom(GameObject room)
     {
-        // this function generates all the rooms and hallways for this Leaf and all of its children.
-        if (leftChild != null || rightChild != null) {
-            // this leaf has been split, so go into the children leafs
-            if (leftChild != null) {
-                leftChild.createRooms();
-            }
-            if (rightChild != null) {
-                rightChild.createRooms();
-            }
-        }
-        else {
-            // this Leaf is the ready to make a room
-            room = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            Vector3 roomSize;
-            Vector3 roomPos;
-            // the room can be between 3 x 3 tiles to the size of the leaf - 2.
-            room.transform.localScale = new Vector3(Random.Range(10, width - 2), Random.Range(10, height - 2));
-            // place the room within the Leaf, but don't put it right 
-            // against the side of the Leaf (that would merge rooms together)
-            room.transform.position = new Vector3(Random.Range(1,room.transform.position.x - (room.transform.localScale.x / 2)), room.transform.position.y,
-                                                Random.Range(1, room.transform.position.z - (room.transform.localScale.z/2)));
-
-            //= new Vector3(Random.Range(1, width - room.transform.localScale.x - 1), Random.Range(1, height - room.transform.localScale.z - 1));
-            room.GetComponent<Renderer>().material.color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
-
-        }
+        this.room = room;
     }
 
-}
+    /// <summary>
+    /// iterate all the way through these leafs to find a room, if one exists.
+    /// </summary>
+    /// <returns></returns>
+    public GameObject GetRoom()
+    {
+        if (room != null)
+            return room;
 
+        GameObject lRoom = null;
+        GameObject rRoom = null;
+
+        if (leftChild != null)
+            lRoom = leftChild.GetRoom();
+
+        if (rightChild != null)
+            rRoom = rightChild.GetRoom();
+
+        if (lRoom == null && rRoom == null)
+            return null;
+
+        if (rRoom == null)
+            return lRoom;
+
+        if (lRoom == null)
+            return rRoom;
+
+        return Random.value > 0.5F ? lRoom : rRoom;
+    }
+
+    #endregion
+
+    #endregion
+}
--- PCG/Assets/Scripts/TreeStructure.cs
+++ PCG/Assets/Scripts/TreeStructure.cs
@@ -4,54 +4,243 @@ using System.Collections.Generic;
 
 public class TreeStructure : MonoBehaviour
 {
+    private const int MaxLeafSize = 35;
 
-    private const int MAX_LEAF_SIZE = 20;
+    [SerializeField] private int worldWidth = 100;
+    [SerializeField] private int worldLenght = 100;
 
-    private List<Node> nodes;
-    private Node root;
+    [SerializeField] private List<Node> nodes;
+    [SerializeField] private Node root;
+
     private Node helperNode;
 
 
-    void Start ()
+    void Start()
     {
-        CreatNodes();
+        CreateNodes();
+        CreateRooms(root);
+    }
 
-	}
-
-
-    void CreatNodes()
+    public void OnDrawGizmos()
     {
-        nodes = new List<Node>();
-       
-        root = new Node(0,0,100,100);
-        nodes.Add(root);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(new Vector3(worldWidth*0.5F, worldLenght*0.5F, 0.0F), new Vector3(worldWidth, worldLenght, 1.0F));
 
+        //for (int i = 0; i < nodes.Count; i++) {
+        //    Gizmos.color = new Color(Random.Range(0.0F, 1.0F), Random.Range(0.0F, 1.0F), Random.Range(0.0F, 1.0F));
+        //    Gizmos.DrawCube(new Vector3(nodes[i].PosX + nodes[i].Width*0.5F, nodes[i].PosZ + nodes[i].Height*0.5F, 0.0F), new Vector3(nodes[i].Width, nodes[i].Height, 1.0F));
+        //}
+    }
 
+    private void CreateNodes()
+    {
+        root = new Node(0, 0, worldWidth, worldLenght);
+        nodes = new List<Node> {root};
 
-        bool did_split = true;
+        bool didSplit = true;
+
         // we loop through every Leaf in our Vector over and over again, until no more Leafs can be split.
-        while (did_split)
-        {
-            did_split = false;
-            for (int i = 0; i < nodes.Count; i++)
-            {
-                if (root.leftChild == null && root.rightChild == null) // if this Leaf is not already split...
+        while (didSplit) {
+            didSplit = false;
+
+            List<Node> copyNodes = new List<Node>(nodes);
+            foreach (var node in nodes) {
+                if (node.LeftChild == null && node.RightChild == null) // if this Leaf is not already split...
                 {
-                    helperNode = nodes[i];
                     // if this Leaf is too big, or 75% chance...
-                    if (helperNode.width > MAX_LEAF_SIZE || helperNode.height > MAX_LEAF_SIZE || Random.Range(0, 2) > 0.25)
-                    {
-                        if (helperNode.split(nodes)) // split the Leaf!
+                    if (node.Width > MaxLeafSize || node.Height > MaxLeafSize || Random.Range(0.0F, 1.0F) > 0.25F) {
+                        if (node.Split()) // split the Leaf!
                         {
                             // if we did split, push the child leafs to the Vector so we can loop into them next
-                            nodes.Add(helperNode.leftChild);
-                            nodes.Add(helperNode.rightChild);
-                            did_split = true;
+                            copyNodes.Add(node.LeftChild);
+                            copyNodes.Add(node.RightChild);
+
+                            didSplit = true;
                         }
                     }
                 }
             }
+            nodes = copyNodes;
         }
+    }
+
+    const int RiverSize = 1;
+
+    private void CreateRooms(Node rootNode)
+    {
+        // this function generates all the rooms and hallways for this Leaf and all of its children.
+        if (rootNode.LeftChild != null || rootNode.RightChild != null) {
+            // this leaf has been split, so go into the children leafs
+            if (rootNode.LeftChild != null) {
+                CreateRooms(rootNode.LeftChild);
+            }
+            if (rootNode.RightChild != null) {
+                CreateRooms(rootNode.RightChild);
+            }
+
+            // if there are both left and right children in this Leaf, create a hallway between them
+            if (rootNode.LeftChild != null && rootNode.RightChild != null)
+            {
+                CreateBridges(rootNode.LeftChild.GetRoom(), rootNode.RightChild.GetRoom());
+            }
+        }
+        // this Leaf is the ready to make a room
+        else {
+            // the room can be between 3 x 3 tiles to the size of the leaf - 2.
+            Vector3 roomSize = new Vector3(rootNode.Width - RiverSize, rootNode.Height - RiverSize, 1.0F);
+            // place the room within the Leaf, but don't put it right 
+            // against the side of the Leaf (that would merge rooms together)
+            Vector3 roomPos = new Vector3(rootNode.PosX, rootNode.PosZ);
+
+            GameObject room = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            room.transform.position = new Vector3(roomPos.x + roomSize.x*0.5F, roomPos.y + roomSize.y*0.5F, 0.0F);
+            room.transform.localScale = roomSize;
+            room.GetComponent<Renderer>().material.color = new Color(Random.Range(0.0F, 1.0F), Random.Range(0.0F, 1.0F), Random.Range(0.0F, 1.0F));
+
+            room.name = "Room";
+            room.transform.SetParent(GameObject.Find("Rooms").transform);
+
+            rootNode.SetRoom(room);
+        }
+    }
+
+    /// <summary>
+    ///        now we connect these two rooms together with hallways.
+    ///        this looks pretty complicated, but it's just trying to figure out which point is where 
+    ///        and then either draw a straight line, or a pair of lines to make a right-angle to connect them.
+    ///        you could do some extra logic to make your halls more bendy, or do some more advanced things if you wanted.
+    /// </summary>.
+    /// <param name="r"></param>
+    /// <param name="l"></param>
+    public void CreateBridges(GameObject r, GameObject l)
+    {
+        List<GameObject> halls = new List<GameObject>();
+
+        //Vector3 point1 = new Vector3(Random.Range(GetLeft(l) /*+ 1*/, GetRight(l) /*- 2*/), Random.Range(GetBottom(l) /*- 2*/, GetTop(l) /*+ 1*/));
+        //Vector3 point2 = new Vector3(Random.Range(GetLeft(r) /*+ 1*/, GetRight(r) /*- 2*/), Random.Range(GetBottom(r) /*- 2*/, GetTop(r) /*+ 1*/));
+
+        Vector3 point1 = l.transform.position;
+        Vector3 point2 = r.transform.position;
+
+        //TODO: DEBUG!
+        GameObject rect = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        rect.transform.position = new Vector3(point1.x, point1.y, -1.0F);
+        rect.transform.localScale = Vector3.one*3;
+
+        GameObject rect2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        rect2.transform.position = new Vector3(point2.x, point2.y, -1.0F);
+        rect2.transform.localScale = Vector3.one * 3;
+
+        Color color = new Color(Random.value, Random.value, Random.value);
+        rect.GetComponent<Renderer>().material.color = color;
+        rect2.GetComponent<Renderer>().material.color = color;
+
+
+
+        float w = point2.x - point1.x;
+        float h = point2.y - point1.y;
+
+        if (w < 0) {
+            if (h < 0) {
+                if (Random.value < 0.5F) {
+                    halls.Add(CreateRectangle(new Vector3(point2.x, point1.y), new Vector3(Mathf.Abs(w), 1)));
+                    halls.Add(CreateRectangle(new Vector3(point2.x, point2.y), new Vector3(1, Mathf.Abs(h))));
+                }
+                else {
+                    halls.Add(CreateRectangle(new Vector3(point2.x, point2.y), new Vector3(Mathf.Abs(w), 1)));
+                    halls.Add(CreateRectangle(new Vector3(point1.x, point2.y), new Vector3(1, Mathf.Abs(h))));
+                }
+            }
+            else if (h > 0) {
+                if (Random.value < 0.5F) {
+                    halls.Add(CreateRectangle(new Vector3(point2.x, point1.y), new Vector3(Mathf.Abs(w), 1)));
+                    halls.Add(CreateRectangle(new Vector3(point2.x, point1.y), new Vector3(1, Mathf.Abs(h))));
+                }
+                else {
+                    halls.Add(CreateRectangle(new Vector3(point2.x, point2.y), new Vector3(Mathf.Abs(w), 1)));
+                    halls.Add(CreateRectangle(new Vector3(point1.x, point1.y), new Vector3(1, Mathf.Abs(h))));
+                }
+            }
+            else // if (h == 0)
+            {
+                halls.Add(CreateRectangle(new Vector3(point2.x, point2.y), new Vector3(Mathf.Abs(w), 1)));
+            }
+        }
+        else if (w > 0) {
+            if (h < 0) {
+                if (Random.value < 0.5F) {
+                    halls.Add(CreateRectangle(new Vector3(point1.x, point2.y), new Vector3(Mathf.Abs(w), 1)));
+                    halls.Add(CreateRectangle(new Vector3(point1.x, point2.y), new Vector3(1, Mathf.Abs(h))));
+                }
+                else {
+                    halls.Add(CreateRectangle(new Vector3(point1.x, point1.y), new Vector3(Mathf.Abs(w), 1)));
+                    halls.Add(CreateRectangle(new Vector3(point2.x, point2.y), new Vector3(1, Mathf.Abs(h))));
+                }
+            }
+            else if (h > 0) {
+                if (Random.value < 0.5F) {
+                    halls.Add(CreateRectangle(new Vector3(point1.x, point1.y), new Vector3(Mathf.Abs(w), 1)));
+                    halls.Add(CreateRectangle(new Vector3(point2.x, point1.y), new Vector3(1, Mathf.Abs(h))));
+                }
+                else {
+                    halls.Add(CreateRectangle(new Vector3(point1.x, point2.y), new Vector3(Mathf.Abs(w), 1)));
+                    halls.Add(CreateRectangle(new Vector3(point1.x, point1.y), new Vector3(1, Mathf.Abs(h))));
+                }
+            }
+            else // if (h == 0)
+            {
+                halls.Add(CreateRectangle(new Vector3(point1.x, point1.y), new Vector3(Mathf.Abs(w), 1)));
+            }
+        }
+        else // if (w == 0)
+        {
+            if (h < 0) {
+                halls.Add(CreateRectangle(new Vector3(point2.x, point2.y), new Vector3(1, Mathf.Abs(h))));
+            }
+            else if (h > 0) {
+                halls.Add(CreateRectangle(new Vector3(point1.x, point1.y), new Vector3(1, Mathf.Abs(h))));
+            }
+        }
+    }
+
+    
+    //TODO: Replace GameObject with data model.
+
+    public GameObject CreateRectangle(Vector3 position, Vector3 size)
+    {
+        GameObject rect = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        rect.transform.position = new Vector3(position.x + size.x/2, position.y + size.y/2, -1.0F);
+        rect.transform.localScale = size;
+
+        rect.name = "Bridge";
+        rect.transform.SetParent(GameObject.Find("Bridges").transform);
+
+        return rect;
+    }
+
+    public float GetLeft(GameObject obj)
+    {
+        GameObject pointer = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        pointer.transform.position = new Vector3(obj.transform.position.x - obj.transform.localScale.x * 0.5F, pointer.transform.position.y, pointer.transform.position.z);
+        pointer.GetComponent<Renderer>().sharedMaterial.color = Color.red;
+        pointer.transform.SetParent(GameObject.Find("Pointers").transform);
+
+        return obj.transform.position.x - obj.transform.localScale.x*0.5F /*- obj.transform.localScale.x*0.5F*/;
+    }
+
+    public float GetRight(GameObject obj)
+    {
+        return obj.transform.position.x + obj.transform.localScale.x*0.5F /*- obj.transform.localScale.x*0.5F*/;
+    }
+
+    public float GetTop(GameObject obj)
+    {
+        return obj.transform.position.y + obj.transform.localScale.y*0.5F /*- obj.transform.localScale.y*0.5F*/;
+    }
+
+    public float GetBottom(GameObject obj)
+    {
+        return obj.transform.position.y - obj.transform.localScale.y*0.5F /*- obj.transform.localScale.y*0.5F*/;
     }
 
 }
